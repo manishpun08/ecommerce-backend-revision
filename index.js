841c5de9c3bd35fbc1f052eeb0404ea1e8f1b26e
arun <arun1nly1@gmail.com>
2024-07-22 17:25:41 +0545

    feat: add product + product list

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 import express from 'express';
 import connectDB from './database-connection/db.connect.js';
+import userRoutes from './user/user.controller.js';
+import productRoutes from './product/product.controller.js';
 
 const app = express();
 
 // to make app understand json
 app.use(express.json());
 
-//TODO: enable CORS
-
 // connect database
 await connectDB();
 
 // register routes
-
-//TODO: handle global error
+app.use(userRoutes);
+app.use(productRoutes);
 
 // network port and server
 const PORT = process.env.PORT;
